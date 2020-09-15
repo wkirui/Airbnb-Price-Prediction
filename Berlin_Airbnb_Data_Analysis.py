267af4d19ce5647a26a00f1500a7195ee8fccae4
Wesley Kirui <kkiruiwesley@gmail.com>
2020-09-15 13:18:28 +0300

    created price distribution chart

--- Berlin_Airbnb_Data_Analysis.py
+++ Berlin_Airbnb_Data_Analysis.py
@@ -9,26 +9,54 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # instantiate app
 def main():
     st.write("""
-             # Welcome to Berlin Airbnb Data Analysis
-             This app uses data from the **Inside Airbnb Project**
+             ## Welcome to Berlin Airbnb Data Analysis
+             This app uses data from the [**Inside Airbnb Project**](http://insideairbnb.com/)
              """)
+     # check top 10 rows of the data
+    listings_data = load_data()
+    st.write("Here is an overview of the airbnb listings data")
+    st.write(listings_data.head(10))
+    
+    # clean up price columns
+    st.write(" Looking at the price values, we can see that they currency denomination in dollars.\
+             We're going to clean them up so that we can start visualizing their distribution")
+    # define a function to clean up price values
+    def clean_prices_column(df,col):
+        df[col] = df[col].str.replace('$','')
+        df[col] = df[col].str.split('.',expand=True)[0]
+        df[col] = df[col].str.replace(',','')
+        df[col] = df[col].astype(int,errors='ignore')
+        return df
+    # create list of columns to clean up
+    price_cols = ['price','weekly_price','monthly_price','extra_people',
+                  'security_deposit','cleaning_fee']
+    for col in price_cols:
+        listings_data_clean = clean_prices_column(listings_data,col)
+    
+    # plot price distribution
+    price_dist_sum = listings_data.groupby('price')['id'].count().rename('total').reset_index().sort_values(by='price',ascending=True)
+    st.write(price_dist_sum['price'].describe())
+    
+    plt.scatter('price','total',data=price_dist_sum)
+    plt.title("Price Distribution")
+    plt.xlabel("Price ($)")
+    plt.ylabel("Total Listings")
+    st.pyplot()
 
 # define function to load data
-@st.cache
+# @st.cache
 def load_data():
     listings_data = pd.read_csv("data/listings.csv.gz",
-                                 compression='gzip',header = 0,sep=',',quotechar='"',error_bad_lines=False)
+                                 compression='gzip',header = 0,sep=',',quotechar='"',error_bad_lines=False,
+                                 low_memory=False)
     return listings_data
 
-# check top rows of the data
-listings_data = load_data()
-st.write("Here is the overview of the the airbnb listings")
-st.write(listings_data.head())
-st.write([x for x in listings_data.columns])
 
 # berlin center: 52.521948, 13.413698
 
