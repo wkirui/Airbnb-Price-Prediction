fc22b37247d9e426017c4666e9eddb3a865db8af
Wesley Kirui <kkiruiwesley@gmail.com>
2020-09-19 01:21:43 +0300

    modified regression mode parameters

--- Berlin_Airbnb_Data_Analysis.py
+++ Berlin_Airbnb_Data_Analysis.py
@@ -290,7 +290,7 @@ def main():
     X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
     # create a model
-    model = RandomForestRegressor()
+    model = RandomForestRegressor(max_depth=5,n_jobs=-1)
     model.fit(X_train,y_train)
     
     # make predictions with the model
