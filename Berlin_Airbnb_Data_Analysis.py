3df0196c3ef91d69263af27c05e168c26ee3b5be
Wesley Kirui <kkiruiwesley@gmail.com>
2020-09-21 12:11:33 +0300

    modified search parameters

--- Berlin_Airbnb_Data_Analysis.py
+++ Berlin_Airbnb_Data_Analysis.py
@@ -365,23 +365,23 @@ def main():
     
     # hyperparameter tuning
      # define parameters to optimize
-     n_estimators = [int(x) for x in np.linspace(start=200,stop=2000,num=50)]
-     max_features = ['auto','sqrt']
-     max_depth = [int(x) for x in np.linspace(10,110,num=11)]
-     max_depth.append(None)
-     min_samples_split = [2,5,10]
-     min_samples_leaf = [1,2,4]
-     bootstrap = [True,False]
+    n_estimators = [int(x) for x in np.linspace(start=50,stop=500,num=20)]
+    max_features = ['auto','sqrt']
+    max_depth = [int(x) for x in np.linspace(10,110,num=11)]
+    max_depth.append(None)
+    min_samples_split = [2,5,10]
+    min_samples_leaf = [1,2,4]
+    bootstrap = [True,False]
      
      # define search grid
-     random_grid = {
+    random_grid = {
          'n_estimators': n_estimators,
          'max_features': max_features,
          'max_depth': max_depth,
          'min_samples_split': min_samples_split,
          'min_samples_leaf': min_samples_leaf,
          'bootstrap': bootstrap
-     }
+        }
     
     # search best parameters
     rf_model = RandomForestRegressor()
